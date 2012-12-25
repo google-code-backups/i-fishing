1d087359510c86cd2f368714f4fb43e712106c9a
greysichatitaly <greysichatitaly@gmail.com>
2012-12-25 10:16:33 +0000

    berkaitan dengan issue menambah berita untuk member ifisihing

--- simpanberita.php
+++ simpanberita.php
@@ -6,25 +6,42 @@
 
 <body>
 <?php
+include "config.php";
 $inputjudul=$_POST['inputJudul'];
 $inputtanggal=$_POST['inputTanggal'];
 $inputwaktu=$_POST['inputWaktu'];
 $inputisi=$_POST['inputIsi'];
-$inputgambar=$_POST['inputGambar'];
-
-$host  ="localhost";
-$user ="root";
-$pwd ="";
-$db_name ="ifishing";
-
-$conn = @mysql_connect ($host, $user, $pwd)
-or die ("Koneksi Gagal, karena " . mysql_error());
-mysql_select_db("$db_name")or die("cannot select database");
-
-$insertdata = "INSERT INTO BERITA (ID_BERITA, JUDUL_BERITA, TANGGAL_BERITA, WAKTU_BERITA, ISI_BERITA, FOTO_BERITA)
-VALUES('','$inputjudul', '$inputtanggal', '$inputwaktu', '$inputisi', '$inputgambar')";
-$hasil=mysql_query($insertdata);
-
+$fileName = $_FILES['inputGambar']['name']; //get the file name
+$fileSize = $_FILES['inputGambar']['size']; //get the size
+$fileError = $_FILES['inputGambar']['error']; //get the error when upload
+if($fileSize > 0 || $fileError == 0)
+{ //check if the file is corrupt or error
+	$move = move_uploaded_file($_FILES['inputGambar']['tmp_name'], 'C:/xampp/htdocs/ifishing/berita/image/'.$fileName); //save image to the folder
+	if($move)
+	{
+		echo "<h3>Success! </h3>";
+		$insertdata = "INSERT INTO BERITA (ID_BERITA, JUDUL_BERITA, TANGGAL_BERITA, WAKTU_BERITA, ISI_BERITA, FOTO_BERITA, FILENAME)
+		VALUES('','$inputjudul', '$inputtanggal', '$inputwaktu', '$inputisi', 'image/$fileName', '$fileName')";
+		$hasil=mysql_query($insertdata);
+		$getfoto = "SELECT FOTO_BERITA from BERITA where FILENAME = '$fileName' limit 1 "; //get the image that have been uploaded
+		$hasilfoto = mysql_query($getfoto);
+		while ($data = mysql_fetch_array($hasilfoto))
+		{
+			$loc = $data['FOTO_BERITA']; 
+?>
+			<br/>
+			<h2> This is the Image : </h2>
+			<img src="<?php echo $loc; ?>" /> <!-- menampilkan gambar-->
+<?php
+		}
+	} else
+		{
+			echo "<h3>Failed! </h3>";
+		}
+} else 
+	{
+		echo "Failed to Upload : ".$fileError;
+	}
 ?>
 <p><center><a>Berita has been saved</a></center></p>
 </body>
